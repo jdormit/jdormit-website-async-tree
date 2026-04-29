e41670e3e3318c8bd3e9c5fbc3e9a7d96a76dd89
jdormit <jeremy.dormitzer@gmail.com>
2026-04-28 23:11:54 -0400

    Add header.html and update baseLayout.ts to include it

--- src/baseLayout.ts
+++ src/baseLayout.ts
@@ -1,10 +1,13 @@
 import { Tree } from "@weborigami/async-tree";
 
+const headerFile = new URL("./header.html", import.meta.url).pathname;
+
 export default async function baseLayout(content: string) {
   return await Tree.indent`
   <!doctype html>
   <html lang="en">
     <head></head>
+    ${await Bun.file(headerFile).text()}
     <body>
      ${content}
     </body>
