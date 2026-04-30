9a8bede5e090d81a7369577ca8c5cc2f7ae3a3c8
Jeremy Dormitzer <jeremydormitzer@hummingbird.co>
2026-04-29 22:14:27 -0400

    Format output files

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,14 @@
+import { $ } from "bun";
 import { FileMap, Tree } from "@weborigami/async-tree";
 import site from "../src/site.ts";
 
 export const build = async () => {
   // Build process writes the site resources to the build folder
-  const buildTree = new FileMap(new URL("../dist", import.meta.url).pathname);
+  const buildDir = new URL("../dist", import.meta.url).pathname;
+  const buildTree = new FileMap(buildDir);
   await Tree.clear(buildTree); // Erase any existing files
   await Tree.assign(buildTree, await site()); // Copy site to build folder
+  await $`bunx --bun prettier --ignore-path='' --write ${buildDir}`;
   console.log("Build complete");
 };
 
